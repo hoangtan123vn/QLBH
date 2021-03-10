e5557975c206aaf09a1ba62d4362c246bf4360bf
TruongNguyenYenNhi <tnynhisgu@gmail.com>
2021-03-10 19:52:54 +0700

    ct2

--- src/main/java/QLBH/HoadonDetailController.java
+++ src/main/java/QLBH/HoadonDetailController.java
@@ -1,29 +1,49 @@
 package QLBH;
+import java.io.IOException;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class HoadonDetailController {
 
-    @FXML
+	@FXML
     private Label lbMahoadon;
-
-    @FXML
-    private Label lbMasanpham;
-
-    @FXML
-    private Label lbSoluong;
-
-    @FXML
-    private Label lbTonggia;
 
     @FXML
     private Label lbThoigianmua;
 
     @FXML
-    private Label lbMakhachhang;
+    private Label lbTonggia;
 
     @FXML
-    private Label lbManhanvien;
+    private Label lbMakh;
+
+    @FXML
+    private Label lbManv;
+    
+    public void setHoadon (Hoadon hoadon) {
+    	lbMahoadon.setText(hoadon.getMahoadon());
+    	lbThoigianmua.setText(hoadon.getThoigianmua());
+    	lbTonggia.setText(String.valueOf(hoadon.getMahoadon()));
+    	lbMakh.setText(String.valueOf(hoadon.getMakh()));
+    	lbManv.setText(String.valueOf(hoadon.getManv()));
+    }
+    
+    @FXML
+    void goBack(ActionEvent e) throws IOException {
+        Stage stage = (Stage)((Node) e.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("danhsachnhanvien.fxml"));
+        Parent QLBHParent = loader.load();
+        Scene scene = new Scene(QLBHParent);
+        stage.setScene(scene);
+
+    }
 
 }
