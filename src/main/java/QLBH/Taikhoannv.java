269d63e48bfc7919a5be55f8d46d17041e6fcd22
hoangtan123vn <tanle271100@gmail.com>
2021-03-14 00:14:05 +0700

    thêm tài khoản nhân viên

--- src/main/java/QLBH/Taikhoannv.java
+++ src/main/java/QLBH/Taikhoannv.java
@@ -6,34 +6,46 @@ import javax.persistence.*;
 import org.hibernate.annotations.GenericGenerator;
 @Entity
 @Table(name="taikhoannv")
-public class Taikhoannv implements Serializable{
+public class Taikhoannv {
 	
-	@GeneratedValue
-	private int manv;
+	
+	
+//	private int manv;
 	@Id
 	private String username;
-	@Id
+	
+	@Column(name="password")
 	private String password;
+	
+	@JoinColumn(name="manv",nullable = false)
+	@OneToOne(cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Nhanvien nhanvien;
 	
 	public Taikhoannv() {
 		super();
 	}
-	public Taikhoannv(String username, String password,int manv) {
+/*	public Taikhoannv(String username, String password,int manv) {
 		super();
 		this.manv=manv;
 		this.username=username;
 		this.password=password;
-	}
+	}*/
 	public Taikhoannv(String username, String password) {
 		this.username=username;
 		this.password=password;
 	}
-	public int getmanv() {
+	public Taikhoannv(String username,String password,Nhanvien nhanvien) {
+		this.username=username;
+		this.password=password;
+		this.nhanvien=nhanvien;
+	}
+/*	public int getmanv() {
 		return manv;
 	}
 	public void setmanv(int manv) {
 		this.manv = manv;
-	}
+	}*/
 	public String getusername() {
 		return username;
 	}
@@ -46,9 +58,10 @@ public class Taikhoannv implements Serializable{
 	public void setpassword(String password) {
 		this.password = password;
 	}
-
-	
-
-	
-	
+	public Nhanvien getNhanvien() {
+		return nhanvien;
+	}
+	public void setNhanvien(Nhanvien nhanvien) {
+		this.nhanvien = nhanvien;
+	}	
 }
