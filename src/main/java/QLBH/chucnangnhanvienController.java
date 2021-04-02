31cdc9620c16ce6cd741525b3eb9d801cc3f4041
huy Từ <72796493+cheesehuy@users.noreply.github.com>
2021-04-02 11:12:21 +0700

    🥺🥺🥺🥺🥺🥺🥺😂😂😒😒❤️😍☺️😘😭😩💕😔😏😁😳😳😉😉

--- src/main/java/QLBH/chucnangnhanvienController.java
+++ src/main/java/QLBH/chucnangnhanvienController.java
@@ -5,13 +5,16 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.URL;
 import org.hibernate.query.Query;
-import org.hibernate.query.criteria.internal.expression.function.AggregationFunction.SUM;
 
+import QLBH.*;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import javax.imageio.ImageIO;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.swing.event.ChangeListener;
 
 import org.apache.derby.iapi.store.access.SpaceInfo;
 import org.hibernate.Session;
@@ -20,6 +23,7 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
 
 import QLBH.Sanpham;
 import javafx.beans.property.ReadOnlyObjectWrapper;
@@ -27,10 +31,15 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -40,14 +49,17 @@ import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import javafx.util.Callback;
 import javafx.scene.control.cell.PropertyValueFactory;
 
@@ -59,9 +71,20 @@ import javafx.scene.control.TableView;
 public class chucnangnhanvienController implements Initializable{
 	
 	
+	@FXML
+	private Label thongbao;
 	 @FXML
 	private TextField tongtien;
-
+	 
+	 @FXML
+	private TextField giamgia;
+	 
+	 @FXML
+	private Label khachhangg;
+	 
+	 @FXML
+	private Label diemtichluy;
+	 
 	@FXML
 	private TextField khachtra;
 
@@ -109,7 +132,17 @@ public class chucnangnhanvienController implements Initializable{
     
     @FXML
     private TableColumn hinhanhsanpham;
-
+    @FXML
+    private Button thanhtoan;
+    
+    @FXML
+    private Button timkhachhang;
+    
+    @FXML
+    private Button chinhsachkhuyenmai;
+    
+   //
+ 
     
     ObservableList<Sanpham> danhmuchoadon = FXCollections.observableArrayList();
     
@@ -124,25 +157,15 @@ public class chucnangnhanvienController implements Initializable{
 		
 			
 		 });
-		 hoadon.setOnMouseClicked(event -> {
-			 //
-			
-			 Sanpham sp = hoadon.getItems().get(hoadon.getSelectionModel().getSelectedIndex());
-			 tongtien.setText(Integer.toString(sp.getGiatien()));
-		
-			
-		 });
+//		 hoadon.setOnMouseClicked(event -> {
+//			 //
+//			
+//			 Sanpham sp = hoadon.getItems().get(hoadon.getSelectionModel().getSelectedIndex());
+//			 tongtien.setText(Integer.toString(sp.getGiatien()));
+//		
+//			
+//		 });
 	 }
-	
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
 //
 	 private void thanhtien() {
 	      
@@ -221,6 +244,8 @@ public class chucnangnhanvienController implements Initializable{
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		
+
+		
 		setCellValueFromTabletoTexfFieldd();
 	//	masanpham.setCellValueFactory(new PropertyValueFactory<Sanpham, Integer>("masanpham"));
 		tensanpham.setCellValueFactory(new PropertyValueFactory<Sanpham, String>("tensanpham"));
@@ -267,8 +292,30 @@ public class chucnangnhanvienController implements Initializable{
 	        });
 		
 //	        
-	    
-	
+		khachtra.addEventHandler(KeyEvent.KEY_PRESSED, e->{
+			if(e.getCode() == KeyCode.ENTER) {
+				if( Integer.parseInt(khachtra.getText()) < Integer.parseInt(tongtien.getText())) {
+				
+                 
+                 khachtra.setText("");
+                 thongbao.setText("Khong Du Tien, Nhap lai");
+                 
+
+                 
+                 
+			}
+				else {
+					 float tiengiam = Float.parseFloat(giamgia.getText())/100;
+					 float tongtiengiam = Float.parseFloat(tongtien.getText())*(1-tiengiam);
+					 float tientrakhach = Float.parseFloat(khachtra.getText())- tongtiengiam;
+					// System.out.print(tiengiam);
+	                // System.out.print(tongtiengiam);
+	                 tienthua.setText(String.valueOf(tientrakhach));
+				}
+			}
+		});
+//
+
 		
 	} 
 	public ObservableList<Sanpham> getSanpham() {
@@ -292,114 +339,188 @@ public class chucnangnhanvienController implements Initializable{
 	//
 	private void event() {
 		 Sanpham sp = TableSP.getItems().get(TableSP.getSelectionModel().getSelectedIndex());
-		 int masanpham = sp.getMasanpham();
-		 String tensanpham = sp.getTensanpham();
-		 String loaisanpham = sp.getLoaisanpham();
-		 String donvi = sp.getDonvi();
-		 int giatien = sp.getGiatien();
-		 addItem(masanpham, tensanpham, loaisanpham, donvi, giatien, 1);
-		 
-		 
-		 
-		// TableView<Integer> table = ... 
+		
 		// Sanpham sp1 = TableSP.getItems().get(TableSP.getSelectionModel().getSelectedIndex());
-		/* if(!hoadon.getItems().contains(sp)) {
+		 if(!hoadon.getItems().contains(sp)) {
 			 sp.setDonvitinh(1);
 			 hoadon.getItems().add(sp);
 			// thanhtien.setOnEditCommit(String.valueOf(sp.getDonvitinh() * sp.getGiatien()));
 			 hoadon.refresh();
 			// Callback cellFactory = null;
 			// xoaSP.setCellFactory(cellFactory);
-
-		
+			 tinhtongtien();
 		
 		 }
 		 
 		 
 		 else if(hoadon.getItems().contains(sp)) {
 			 sp.setDonvitinh(sp.getDonvitinh()+1);
-			 hoadon.refresh();*/
-				tienthua.addEventHandler(KeyEvent.KEY_PRESSED, e->{
-					if(e.getCode() == KeyCode.ENTER) {
-						
-						 int tientrakhach = Integer.parseInt(khachtra.getText())- Integer.parseInt(tongtien.getText());
-		                 System.out.print(tientrakhach);
-					}
-				});
-				hoadon.refresh();
-		 }
-	
-			
-	//	 int soluong = sp.getDonvitinh();
-	/*	int masanpham = sp.getMasanpham();
-		String tensanpham = sp.getTensanpham();
-		String loaisanpham = sp.getLoaisanpham();
-		String donvi = sp.getDonvi();
-		int giatien = sp.getGiatien();
-		//int giatien
-		 int soluong = 1 ;
-		 for(Sanpham sanpham : danhmuchoadon) {
-			 if(sanpham.getMasanpham()==sp.getMasanpham()) {
-				// int capnhatsoluong = 
-				 soluong = soluong +1;
-				 hoadon.getItems().set(hoadon.getItems().indexOf(sanpham),sanpham);
-				 hoadon.refresh();
-				//return;
-			 }
-		 }
-		 
-		 
-		 danhmuchoadon.add(new Sanpham(masanpham,tensanpham,loaisanpham,donvi,giatien,soluong));
-		 if(!danhmuchoadon.add(new Sanpham(masanpham,tensanpham,loaisanpham,donvi,giatien,soluong))) {
-		 }
-		 hoadon.setItems(danhmuchoadon);*/
-		 
-		 
-		 /*	 
-	 if(!hoadon.getItems().contains(sp)) {
-			// sp.setDonvitinh(1);
-			 hoadon.getItems().add(sp);
-			// thanhtien.setOnEditCommit(String.valueOf(sp.getDonvitinh() * sp.getGiatien()));
 			 hoadon.refresh();
-			// Callback cellFactory = null;
-			// xoaSP.setCellFactory(cellFactory);
+			 tinhtongtien();
 
+		 }
+	}
+	public void tinhtongtien() {
+		Sanpham sp = TableSP.getItems().get(TableSP.getSelectionModel().getSelectedIndex());
+		 thanhtien.setCellFactory(new Callback<TableColumn, TableCell>() {
+	            @Override
+	            public TableCell call(TableColumn p) {
+	                return new TableCell() {
+	                    @Override
+	                    protected void updateItem(Object item, boolean empty) {
+	                        super.updateItem(item, empty);
+	                        if (this.getTableRow() != null && item != null) {
+//	                            System.out.println(this.getTableRow().getItem().toString());
+//	                            setText((this.getTableRow().getIndex() + 1) + "");
+//	                              System.out.println(this.getTableRow().getIndex());
+//	                            System.out.println(getTableView().getItems().get(getTableRow().getIndex() -1));
+//	                            System.out.println();
+	                           
+	                            System.out.println("");
+//	                            setText(String.valueOf(Integer.parseInt(getTableRow().getItem().toString()) + Integer.parseInt(getTableView().getItems().get(pre).toString())));
+	                            Integer totalValue = new Integer(0);
+	                            totalValue = sp.getDonvitinh() * sp.getGiatien();
+	                            setText(String.valueOf(totalValue));
+	                            int sum = 0;
+	                            for (Sanpham thanhtien : hoadon.getItems()) {
+	                                sum = sum + (thanhtien.getGiatien()* thanhtien.getDonvitinh());
+	                            }
+	                            tongtien.setText(String.valueOf(sum));
+	                           
+	                           //int tientrakhach = Integer.parseInt(khachtra.getText())- Integer.parseInt(tongtien.getText());
+	                         //    System.out.print(tientrakhach);
+//	                            setText(this.getTableRow().getItem().toString());
+	                        } else {
+	                            setText("");
+	                        }
+	                    }
+	                };
+	            }
+	        }); 
+	}
+	//tim kiem 
+	@FXML
+	private TextField Timkiem;
+	void Timkiem() {
+		ObservableList<Sanpham> TableSP = FXCollections.observableArrayList(getSanpham());
+
+		FilteredList<Sanpham> filteredData = new FilteredList<>(TableSP, b -> true);
+		Timkiem.textProperty().addListener((observable, oldValue, newValue) -> {
+			filteredData.setPredicate(sanpham -> {
+				if (newValue == null || newValue.isEmpty()) {
+					return true;
+				}
+				String lowerCaseFilter = newValue.toLowerCase();
+
+				if (sanpham.getTensanpham().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+					return true; // Filter matches username
+				} else if (sanpham.getDonvi().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+					return true; // Filter matches password
+				} else
+					return false; // Does not match.
+			});
+		});
+		SortedList<Sanpham> sortedData = new SortedList<>(filteredData);
+		sortedData.comparatorProperty().bind(((TableView<Sanpham>) TableSP).comparatorProperty());
+		((TableView<Sanpham>) TableSP).setItems(sortedData);
+
+	}
+	/// thanh toan
+	@FXML
+	void thanhtoan(ActionEvent event) {
+		//Sanpham sp = new Sanpham(10,"A","a","s",1000,2000);
+		//sp.getMasanpham();
 		
+
+
+		Alert alert = new Alert(AlertType.INFORMATION);
+		StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+				.configure("hibernate.cfg.xml")
+				.build();
+		Metadata metaData = new MetadataSources(standardRegistry).getMetadataBuilder().build();
+		SessionFactory sessionFactory = metaData.getSessionFactoryBuilder().build();
+		Session session = sessionFactory.openSession();
+
+		int makh = Integer.parseInt(khachhangg.getText());
+		int tongtienmua = Integer.parseInt(tongtien.getText());
+		KhachHang khachhang = new KhachHang();
+		khachhang = session.get(KhachHang.class, makh);
+		Hoadon hoadonn = new Hoadon(null, tongtienmua, null,khachhang);
+		 try {
+    		 session.beginTransaction();
+    		 session.save(hoadonn);
+    		 hoadonn = session.get(Hoadon.class, hoadonn.getMahoadon());
+    		 //session.save(chitiethoadon);
+    		// session.save(spp);
+    		 session.getTransaction().commit();
+    		 alert.setContentText("Thêm hoa don thành công !");
+    		 alert.showAndWait();   
+		 }
+	    	catch (RuntimeException error){
+	    		
+	    		 alert.setContentText("Thêm hoa don thất bại   !");
+	    		 alert.showAndWait();
+	    		session.getTransaction().rollback();
+	    	}
+		for (Sanpham sp : hoadon.getItems()) {
+			Sanpham spp= new Sanpham();
+			int masp = sp.getMasanpham();
+			spp = session.get(Sanpham.class, masp);
+			int soluong = sp.getDonvitinh();
+			System.out.println(masp);
+			//System.out.println(masp);
+			Chitiethoadon  chitiethoadon = new Chitiethoadon(soluong,hoadonn,spp);
+			 try {
+	    		 session.beginTransaction();
+	    		// session.save(hoadonn);
+	    		 hoadonn = session.get(Hoadon.class, hoadonn.getMahoadon());
+	    		 session.save(chitiethoadon);
+	    		// session.save(spp);
+	    		 session.getTransaction().commit();
+	    		 alert.setContentText("Thêm chi tiet  thành công !");
+	    		 alert.showAndWait();   
+			 }
+		    	catch (RuntimeException error){
+		    		
+		    		 alert.setContentText("Thêm chi tiet thất bại   !");
+		    		 alert.showAndWait();
+		    		session.getTransaction().rollback();
+		    	}
+			}
 		
-		 }*/
-		// int soluong = sp.getDonvitinh();
-		 
+}
+
+	
+	// tim khach hang
+	@FXML
+	void timkhachhang(ActionEvent event) throws IOException{
+		Parent root = FXMLLoader.load(getClass().getResource("timkhachhang.fxml"));
+		Scene scene = new Scene(root);
+		Stage stage = new Stage();
+		stage.setScene(scene);
+		stage.show();
+	}
+	@FXML
+	void chinhsachkhuyenmai(ActionEvent event) {
 		
-	public void addItem(int masanpham,String tensanpham, String loaisanpham, String donvi,int giatien,int donvitinh) {
-	    Sanpham entry = hoadon.getItems().stream()
-	        .filter(item -> item.getTensanpham().equals(tensanpham))
-	        .findAny()
-	        .orElseGet(()-> {
-	             Sanpham newItem = new Sanpham(masanpham,tensanpham, loaisanpham,donvi,giatien, 0);
-	             hoadon.getItems().add(newItem);
-	             return newItem ;
-	        });
-	    
-	    
-	    entry.setDonvitinh(entry.getDonvitinh() + donvitinh);
- 	    entry.setGiatien(entry.getGiatien() + giatien);
- 	   // TableSP.refresh();
-	   // entry.s
 	}
 	
-	/*   public void calculateTotal() {
-	        ObservableList<Sanpham> items = hoadon.getItems();
-
-	        double total = 0.0;
-
-	        for (Sanpham item : items) {
-	            total += item.getTotal();
-	        }
-
-	        lblTotal.setText("Total : " + total + "");
-	    }*/
-
-	
-	
-	
+	public void setkhachhang(KhachHang khachhang) {
+		//khachhangg.setText(khachhang.getMakh().toString());
+		//thongbao.setText(khachhang.getMakh().toString());
+		diemtichluy.setText(String.valueOf((khachhang.getDiemtichluy())));
+		khachhangg.setText(String.valueOf(khachhang.getMakh()));
+		if(Integer.parseInt(diemtichluy.getText())> 1000) {
+			giamgia.setText("20");
+		}
+		else if (Integer.parseInt(diemtichluy.getText())< 1000 && Integer.parseInt(diemtichluy.getText())>=500) {
+			giamgia.setText("10");
+		}
+		else if (Integer.parseInt(diemtichluy.getText())< 500 && Integer.parseInt(diemtichluy.getText())> 100) {
+			giamgia.setText("5");
+		}
+		else giamgia.setText("0");
+	}
 }
+
+		 
