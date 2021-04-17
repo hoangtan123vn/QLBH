5ddc0fd3c7240445556dd03bcb4bfc1a7f1e95a8
huy Từ <72796493+cheesehuy@users.noreply.github.com>
2021-04-17 21:29:04 +0700

    a

--- src/main/java/BanHang/themkhachhangController.java
+++ src/main/java/BanHang/themkhachhangController.java
@@ -5,6 +5,8 @@ package BanHang;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -25,6 +27,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
@@ -54,10 +57,23 @@ public class themkhachhangController  implements Initializable{
 
     @FXML
     private Button quaylai;
-
+    @FXML
+    private Label checkname;
+    @FXML
+    private Label checkaddress;
+    @FXML
+    private Label checkbirth;
+    @FXML
+    private Label checknumber;
+    @FXML
+    private Label checkgender;
+    @FXML
+    private Label checkmail;
   
+
     @FXML
     void themkhachhang(ActionEvent event) {
+    	if(kiemtrahoten() & kiemtradiachi() & KiemTraSDT() & KiemTraNgaySinh() & KiemTraGioiTinh() & kiemtramail()) { 
     	
     	Alert alert = new Alert(AlertType.INFORMATION);
     	String hoten = name.getText();
@@ -90,7 +106,93 @@ public class themkhachhangController  implements Initializable{
     		session.getTransaction().rollback();
     	
     	}
+		// reset 
+		name.setText(null);
+		address.setText(null);
+		numberphone.setText(null);
+		birth.setValue(null);
+		sex.setValue(null);
+		mail.setText(null);
+    	}
     }
+    private boolean kiemtrahoten() {
+		Pattern p = Pattern.compile("^[A-Za-zÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚÝàáâãèéêìíòóôõùúýĂăĐđĨĩŨũƠơƯưẠ-ỹ ]+$");
+		Matcher m = p.matcher(name.getText());
+		if(m.find() && m.group().equals(name.getText())){
+			checkname.setText(null);
+			return true;
+		}
+		else {
+			checkname.setText("Vui lòng điền tên hợp lệ");
+			return false;
+		}
+	}
+    private boolean kiemtradiachi() {
+		Pattern p = Pattern.compile("^[A-Za-zÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚÝàáâãèéêìíòóôõùúýĂăĐđĨĩŨũƠơƯưẠ-ỹ ]+$");
+		Matcher m = p.matcher(address.getText());
+		if(m.find() && m.group().equals(address.getText())){
+			checkaddress.setText(null);
+			return true;
+		}
+		else {
+			checkaddress.setText("Vui lòng điền tên hợp lệ");
+			return false;
+		}
+	}
+    private boolean KiemTraSDT() {
+		Pattern p = Pattern.compile("[0-9]+");
+		Matcher m = p.matcher(numberphone.getText());
+		if(m.find() && m.group().equals(numberphone.getText())){
+			checknumber.setText(null);
+			return true;
+		}
+		else {
+			checknumber.setText("Vui lòng điền số điện thoại hợp lệ");
+			return false;
+		}
+	}
+    private boolean KiemTraNgaySinh() {
+		if(birth.getValue() == null){
+			checkbirth.setText("Vui lòng điền số ngày ở hợp lệ");
+			return false;
+		}
+		checkbirth.setText(null);
+		return true;
+	}
+    private boolean KiemTraGioiTinh() {
+		if(sex.getValue() == null){
+			checkgender.setText("Vui lòng chọn giới tính nhân viên");
+			return false;
+		}
+		checkgender.setText(null);
+		return true;
+	}
+    
+ 
+
+    public static final Pattern VALIDEMAIL = 
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+        private  boolean kiemtramail() {
+        		
+                Matcher  matcher = VALIDEMAIL.matcher(mail.getText());
+        		if(matcher.find() && matcher.group().equals(mail.getText())){
+        			
+        					
+        				
+        				checkmail.setText(null);
+        				return true;
+        				
+  
+        		}
+        		if(mail.getText().isEmpty()) {
+        			checkmail.setText("mail chua duoc nhap");
+        		}
+        		else
+                checkmail.setText("mail ko hop le");
+                return false;
+        
+        }
 
     @FXML
     void quaylai(ActionEvent event) {
