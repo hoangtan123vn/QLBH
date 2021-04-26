d2f3a08f5d2b8f82971a1892c19dd7f46cc1bf14
sanglqsgu <71993474+sanglqsgu@users.noreply.github.com>
2021-04-27 03:35:19 +0700

    css

--- src/main/java/ThongKe/Main.java
+++ src/main/java/ThongKe/Main.java
@@ -18,6 +18,7 @@ public class Main extends Application {
 		// TODO Auto-generated method stub
 		Parent root = FXMLLoader.load(getClass().getResource("thongkedoanhthu.fxml"));
 		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("thongke.css").toExternalForm());
 		stage.setScene(scene);
 		stage.setTitle("Phan mem quan ly ban hang");
 		stage.show();
