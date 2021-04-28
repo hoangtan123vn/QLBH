483de283fbd4a84eb8ab188c2106ac187aa1cf7e
beoheo17 <64195306+beoheo17@users.noreply.github.com>
2021-04-29 01:26:40 +0700

    aydzo
    lalala

--- src/main/java/Nhacungcap/Main.java
+++ src/main/java/Nhacungcap/Main.java
@@ -7,23 +7,19 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class Main  extends Application {
+public class Main extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
-	//	Parent root = FXMLLoader.load(getClass().getResource("/Nhanvien/nhanvien.fxml"));
-	//	Parent root = FXMLLoader.load(getClass().getResource("chucnangquanly.fxml"));
 		Parent root = FXMLLoader.load(getClass().getResource("nhacungcap.fxml"));
 		Scene scene = new Scene(root);
-	//	scene.getStylesheets().add(getClass().getResource("nhacungcap.css").toExternalForm());
 		stage.getIcons().add(new Image(Main.class.getResourceAsStream("backgroundSGU.png")));
 		stage.setResizable(false);
 		stage.setScene(scene);
 		stage.setTitle("Phan mem quan ly ban hang");
-		stage.show();	
-
+		stage.show();
 	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub 
 		launch(args);
 	}
 
--- src/main/java/Nhacungcap/ThemNCCController.java
+++ src/main/java/Nhacungcap/ThemNCCController.java
@@ -51,196 +51,165 @@ import QLBH.GiaoDienQLController;
 import QLBH.HibernateUtils;
 import QLBH.Nhacungcap;
 
-public class ThemNCCController extends Application implements Initializable{
-/*	public static ThemNCCController instance;
-
-    public ThemNCCController() {
-        instance = this;
-    }
-
-    public static ThemNCCController getInstance() {
-        return instance;
-    }
-*/	
+public class ThemNCCController extends Application implements Initializable {
 	@FXML
-    private TextField tfncc;
+	private TextField tfncc;
 
-    @FXML
-    private TextField tftenncc;
+	@FXML
+	private TextField tftenncc;
 
-    @FXML
-    private TextField tfsdt;
+	@FXML
+	private TextField tfsdt;
 
-    @FXML
-    private TextField tfdiachi;
+	@FXML
+	private TextField tfdiachi;
 
-    @FXML
-    private TextField tfemail;
-    
-    @FXML
-    private TextField tfsotienno;
+	@FXML
+	private TextField tfemail;
 
-    @FXML
-    private Button idsave;
+	@FXML
+	private TextField tfsotienno;
 
-    @FXML
-    private Button idclose;
-    
-    @FXML
-    private ImageView exit;
-    
-    @FXML
-    void exit(MouseEvent event) {
-    	Stage stage = (Stage) exit.getScene().getWindow();
+	@FXML
+	private Button idsave;
+
+	@FXML
+	private Button idclose;
+
+	@FXML
+	private ImageView exit;
+
+	@FXML
+	void exit(MouseEvent event) {
+		Stage stage = (Stage) exit.getScene().getWindow();
 		stage.close();
 		GiaoDienQLController.getInstance().falsedisable();
-    }
+	}
 
-    @FXML
-    void close(ActionEvent event) {
-    	 Stage stage = (Stage) idclose.getScene().getWindow();
-    	 stage.close();
-    	 GiaoDienQLController.getInstance().falsedisable();
-    }
+	@FXML
+	void close(ActionEvent event) {
+		Stage stage = (Stage) idclose.getScene().getWindow();
+		stage.close();
+		GiaoDienQLController.getInstance().falsedisable();
+	}
 
-    @FXML
-    void save(ActionEvent event) {
-    	if(KiemTraSDT() & KiemTraTenNCC()& KiemTraEmail()&KiemTraDiaChi()&KiemTraTien()) {
-    	Alert alert = new Alert(AlertType.INFORMATION);
-		alert.setTitle("Them Nha cung cap");
-		 
-    	//Integer mancc = Integer.parseInt(tfncc.getText());
-    	String tenncc = tftenncc.getText();
-    	String diachi = tfdiachi.getText();
-    	String sodienthoai = tfsdt.getText();
-    	String email = tfemail.getText();
-    	Integer sotienno = Integer.parseInt(tfsotienno.getText());
-    
-    	Session session = HibernateUtils.getSessionFactory().openSession();
-
-    	try {
-    		
-    		 Nhacungcap nv = new Nhacungcap(tenncc,diachi,sodienthoai,email,sotienno);
-    		 session.beginTransaction();
-    		 session.save(nv);
-    		 session.getTransaction().commit();
-    		 //Nhacungcap ncc = new Nhacungcap(tenncc,diachi,sodienthoai,email);
-    		 Stage stage = (Stage) idsave.getScene().getWindow();
-    		 
-        	 stage.close();
-        	 alert.setContentText("Them nha cung cap thanh cong !");
-        	 alert.showAndWait();    
-        	 
-        	 
-        	nhacungcapController.getInstance().ReloadNHACUNGCAP();
-        	GiaoDienQLController.getInstance().falsedisable();
-        	
-    	}
-    	catch (RuntimeException error){
-    		 alert.setContentText("Them nha cung cap that bai  !");
-    		 alert.showAndWait();
-    		session.getTransaction().rollback();
-    	}
-    }
-    }
-    @FXML
-    private Label check_sdt;
-    
-    private boolean KiemTraSDT() {
-        Pattern p = Pattern.compile("[0-9]+");
-        Matcher m = p.matcher(tfsdt.getText());
-        if(m.find() && m.group().equals(tfsdt.getText())){
-            check_sdt.setText(null);
-            return true;
-        }
-        
-        else {
-            check_sdt.setText("Vui lòng điền số điện thoại hợp lệ");
-            return false;
-        }
-    }
-    public boolean isNumber(String s) {
-    	try {
-			Double.parseDouble(s);
-			return true; 
-		} catch (Exception e) {
-			return false;			
+	@FXML
+	void save(ActionEvent event) {
+		if (KiemTraSDT() & KiemTraTenNCC() & KiemTraEmail() & KiemTraDiaChi() & KiemTraTien()) {
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Them Nha cung cap");
+			String tenncc = tftenncc.getText();
+			String diachi = tfdiachi.getText();
+			String sodienthoai = tfsdt.getText();
+			String email = tfemail.getText();
+			Integer sotienno = Integer.parseInt(tfsotienno.getText());
+			Session session = HibernateUtils.getSessionFactory().openSession();
+			try {
+				Nhacungcap nv = new Nhacungcap(tenncc, diachi, sodienthoai, email, sotienno);
+				session.beginTransaction();
+				session.save(nv);
+				session.getTransaction().commit();
+				Stage stage = (Stage) idsave.getScene().getWindow();
+				stage.close();
+				alert.setContentText("Them nha cung cap thanh cong !");
+				alert.showAndWait();
+				nhacungcapController.getInstance().ReloadNHACUNGCAP();
+				GiaoDienQLController.getInstance().falsedisable();
+			} catch (RuntimeException error) {
+				alert.setContentText("Them nha cung cap that bai  !");
+				alert.showAndWait();
+				session.getTransaction().rollback();
+			}
 		}
-    }
-    
-    @FXML
-    private Label check_sotienno;
-    private boolean KiemTraTien() {
-        Pattern p = Pattern.compile("[0-9]+");
-        Matcher m = p.matcher(tfsotienno.getText());
-        if(m.find() && m.group().equals(tfsotienno.getText())){
-            check_sotienno.setText(null);
-            return true;
-        }
-        else {
-            check_sotienno.setText("Vui lòng điền số tiền hợp lệ");
-            return false;
-        }
-    }
-    
-    @FXML
-    private Label check_hoten;
-    
-    private boolean KiemTraTenNCC() {
+	}
+
+	@FXML
+	private Label check_sdt;
+
+	private boolean KiemTraSDT() {
+		Pattern p = Pattern.compile("[0-9]+");
+		Matcher m = p.matcher(tfsdt.getText());
+		if (m.find() && m.group().equals(tfsdt.getText())) {
+			check_sdt.setText(null);
+			return true;
+		} else {
+			check_sdt.setText("Vui lòng điền số điện thoại hợp lệ");
+			return false;
+		}
+	}
+
+	public boolean isNumber(String s) {
+		try {
+			Double.parseDouble(s);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	@FXML
+	private Label check_sotienno;
+
+	private boolean KiemTraTien() {
+		Pattern p = Pattern.compile("[0-9]+");
+		Matcher m = p.matcher(tfsotienno.getText());
+		if (m.find() && m.group().equals(tfsotienno.getText())) {
+			check_sotienno.setText(null);
+			return true;
+		} else {
+			check_sotienno.setText("Vui lòng điền số tiền hợp lệ");
+			return false;
+		}
+	}
+
+	@FXML
+	private Label check_hoten;
+
+	private boolean KiemTraTenNCC() {
 		Pattern p = Pattern.compile("^[A-Za-zÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚÝàáâãèéêìíòóôõùúýĂăĐđĨĩŨũƠơƯưẠ-ỹ ]+$");
 		Matcher m = p.matcher(tftenncc.getText());
-		if(m.find() && m.group().equals(tftenncc.getText())){
+		if (m.find() && m.group().equals(tftenncc.getText())) {
 			check_hoten.setText(null);
 			return true;
-		}
-		else {
+		} else {
 			check_hoten.setText("Vui lòng điền tên hợp lệ");
 			return false;
 		}
 	}
-    
-    @FXML
-    private Label check_email;
-    private boolean KiemTraEmail() {
-    	Pattern p = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
-    	Matcher m = p.matcher(tfemail.getText());
-    	if(m.find() && m.group().equals(tfemail.getText())){
+
+	@FXML
+	private Label check_email;
+
+	private boolean KiemTraEmail() {
+		Pattern p = Pattern.compile(
+				"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+		Matcher m = p.matcher(tfemail.getText());
+		if (m.find() && m.group().equals(tfemail.getText())) {
 			check_email.setText(null);
 			return true;
-		}
-    	else {
+		} else {
 			check_email.setText("Vui lòng điền email hợp lệ");
 			return false;
 		}
-    }
-    
-    @FXML
-    private Label check_diachi;
-    private boolean KiemTraDiaChi() {
-    	//System.out.println(tfdc.getText());
-		if(tfdiachi.getText().isEmpty()){
+	}
+
+	@FXML
+	private Label check_diachi;
+
+	private boolean KiemTraDiaChi() {
+		if (tfdiachi.getText().isEmpty()) {
 			check_diachi.setText("Vui lòng điền địa chỉ phù hợp");
 			return false;
 		}
 		check_diachi.setText(null);
 		return true;
-		}
-   
+	}
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		
 	}
-	/*public void showMessageDialog() {
-		Alert alert = new Alert(AlertType.INFORMATION);
-		 alert.setTitle("Test Connection");
-	}*/
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		
 	}
-
 }
